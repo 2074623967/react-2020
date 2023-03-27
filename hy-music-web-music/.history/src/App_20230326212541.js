07eecb5ce9b77e82a2ec07c5f9685b74c0ead88f
tangchungjiao <2074623967@qq.com>
2023-03-27 21:24:59 +0800

    项目结构的搭建和全局样式、craco配置

---
+++ hy-music-web-music/.history/src/App_20230326212541.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+
+const App = memo((props) => {
+  return (
+    <div>App</div>
+  )
+})
+
+App.propTypes = {}
+
+export default App
